debd15655ab7112dfe2963cec87caacf4462001c
victordiaz74 <vdt21victor@gmail.com>
2022-10-27 10:54:09 +0200

    adapter y recycler creados

--- app/src/main/java/com/example/quizz/room/ListadoPreguntas.kt
+++ app/src/main/java/com/example/quizz/room/ListadoPreguntas.kt
@@ -1,25 +1,43 @@
 package com.example.quizz.room
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.quizz.PreguntaListAdapter
 import com.example.quizz.R
-import com.example.quizz.adapter.PreguntaAdapter
+import com.example.quizz.data.PreguntaViewModel
+import com.example.quizz.data.PreguntaViewModelFactory
+import kotlinx.coroutines.flow.observeOn
 
 class ListadoPreguntas : AppCompatActivity() {
+
+    private val newPreguntaActivityRequestCode = 1
+    private val preguntaViewModel: PreguntaViewModel by viewModels{
+        PreguntaViewModelFactory((application as PreguntaApplication).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listado)
+
         initRecyclerView()
+/*
+        preguntaViewModel.todasLasPreguntas.observeOn(owner = this){
+            preguntas ->
+            pregunta.let{
+                adapter.submitList(it)
+            }
+        }*/
     }
 
     fun initRecyclerView() {
         val rv = findViewById<RecyclerView>(R.id.listado)
+        val adapter = PreguntaListAdapter()
+        rv.adapter = adapter
         rv.layoutManager = LinearLayoutManager(this)
-        val listadoPreguntas = PreguntaDatabase
-        listadoPreguntas.setContext(this)
-        rv.adapter = PreguntaAdapter(listadoPreguntas.filllist())
+
     }
 
 
